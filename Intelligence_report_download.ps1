094d865ada2fff526978d7599d379b39fb40fc3b
tsiripanichpon <127237885+tsiripanichpon@users.noreply.github.com>
2023-03-07 13:07:47 -0500

    Add files via upload

--- Intelligence_report_download.ps1
+++ Intelligence_report_download.ps1
@@ -1,94 +1,97 @@
-#!/usr/bin/env python
-# coding: utf-8
+# This script gets the latest data from an existing Workspace ONE Intelligence report and download the report as a CSV.
 
-import requests
-import json
-import csv
-import argparse
-from datetime import datetime
-import time
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-Region", help="Workspace ONE Intelligence API region. More information here: https://docs.vmware.com/en/VMware-Workspace-ONE/services/intelligence-documentation/GUID-04_intel_reqs.html", required=True)
-parser.add_argument("-ReportId", help="report id", required=True)
-parser.add_argument("-ClientId", help="client id", required=True)
-parser.add_argument("-ClientSecret", help="client secret", required=True)
-parser.add_argument("-OutFile", help="output file name", required=True)
-args = parser.parse_args()
-
-region = args.Region
-report_id = args.ReportId
-client_id = args.ClientId
-client_secret = args.ClientSecret
-out_file = args.OutFile
+# Now you can use the script by calling it and passing the required parameters like this:
+# .\Get-IntelligenceReport.ps1 -Region "sandbox" -ReportId "XXXXXXX" -ClientId "XXXXXXX" -ClientSecret "XXXXXX" -OutFile "XXXXXXXXX_$(Get-Date -Format "yyyyMMddHHmmss").csv"
 
 
-def get_access_token(url, client_id, client_secret):
-    try:
-        data = {'grant_type': 'client_credentials', 'client_id': client_id, 'client_secret': client_secret}
-        response = requests.post(url, data=data)
-        response.raise_for_status()
-        return response.json()['access_token']
-    except requests.exceptions.RequestException as e:
-        print("Error getting access token:", e)
-        return None
 
-def run_report(report_id, url, access_token):
-    try:
-        url_run = url + report_id + "/run"
-        headers = {'Authorization': 'Bearer ' + access_token, 'content-type': 'application/json'}
-        response = requests.post(url_run, headers=headers)
-        response.raise_for_status()
-        print("Running report...")
-        time.sleep(60) # add delays to prevent race condition where reports are not yet generated but report list may be retrieved
-    except requests.exceptions.RequestException as e:
-        print("Error running report:", e)
+param (
+    [string]$Region,
+    [string]$ReportId,
+    [string]$ClientId,
+    [string]$ClientSecret,
+    [string]$OutFile
+)
 
-def get_report_list(report_id, url, access_token):
-    try:
-        url_report_list = url + report_id + "/downloads/search"
-        headers = {'Authorization': 'Bearer ' + access_token, 'content-type': 'application/json'}
-        data = {'offset': '', 'page_size': 200}
-        response = requests.post(url_report_list, headers=headers, data=json.dumps(data))
-        response.raise_for_status()
-        report_list_json = response.json()['data']['results']
-        report_list_json.sort(key=lambda x: x['start_time'], reverse=True)
-        print("Getting report list...")
-        return report_list_json
-    except requests.exceptions.RequestException as e:
-        print("Error getting report list:", e)
-        return None
+function Get-AccessToken($url, $client_id, $client_secret) {
+    try {
+        $response = Invoke-RestMethod -Uri $url -Method Post -Body @{grant_type="client_credentials"; client_id=$client_id; client_secret=$client_secret} -ErrorAction Stop
+        return $response.access_token
+    } catch {
+        Write-Error "Error getting access token: $($_.Exception.Message)"
+        return $null
+    }
+}
 
-def download_report(scheduled_id, url, access_token):
-    try:
-        url_download = url + 'tracking/' + scheduled_id + "/download"
-        headers = {'Authorization': 'Bearer ' + access_token, 'content-type': 'application/json'}
-        response = requests.get(url_download, headers=headers)
-        response.raise_for_status()
-        print("Downloading report...")
-        return response.content
-    except requests.exceptions.RequestException as e:
-        print("Error downloading report:", e)
-        return None
+function Run-Report($report_id, $url, $access_token) {
+    try {
+        $url_run = $url + $report_id + "/run"
+        $headers = @{
+            'Authorization' = "Bearer " + $access_token
+            'content-type' = "application/json"
+        }
+        Invoke-RestMethod -Uri $url_run -Method Post -Headers $headers -ErrorAction Stop
+	    Write-Progress -Activity "Running report" -Status "Report is being generated" -PercentComplete 25
+	    Start-Sleep -Seconds 30 # add delays to prevent race condition where reports are not yet generated but report list may be retrieved
+    } catch {
+        Write-Error "Error running report: $($_.Exception.Message)"
+    }
+}
 
-def save_report(out_file_name, downloaded_report):
-    try:
-        with open(out_file_name, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(csv.reader(downloaded_report.decode('utf-8').splitlines()))
-        print("Saving report...")
-    except IOError as e:
-        print("Error saving report:", e)
+function Get-Report-List($report_id, $url, $access_token) {
+    try {
+        $url_report_list = $url + $report_id + "/downloads/search"
+        $headers = @{
+            'Authorization' = "Bearer " + $access_token
+            'content-type' = "application/json"
+        }
+        $body = '{"offset":"","page_size":200}'
+        $report_list = Invoke-RestMethod -Uri $url_report_list -Method Post -Headers $headers -Body $body -ErrorAction Stop
+        $report_list_json = $report_list.data.results
+        $report_list_json | Sort-Object -Property start_time -Descending
+	    Write-Progress -Activity "Getting report list" -Status "Report list retrieved" -PercentComplete 50
+        return $report_list_json
+    } catch {
+        Write-Error "Error getting report list: $($_.Exception.Message)"
+        return $null
+    }
+}
 
-def main(region, report_id, client_id, client_secret, out_file):
-    base_api_url = "https://api." + region + ".data.vmwservices.com/v1/reports/"
-    base_auth_url = "https://auth." + region + ".data.vmwservices.com/oauth/token"
-    access_token = get_access_token(base_auth_url, client_id, client_secret) # generate access token
-    run_report(report_id, base_api_url, access_token) # run report
-    report_list = get_report_list(report_id, base_api_url, access_token) # get the list of all available reports for downloads
-    scheduled_id = report_list[0]['id'] # get the latest available report
-    downloaded_report = download_report(scheduled_id, base_api_url, access_token) # download report
-    save_report(out_file, downloaded_report) # save report
-    
-if __name__ == "__main__":
-    main(region, report_id, client_id, client_secret, out_file)
+function Download-Report($scheduled_id, $url, $access_token) {
+    try {
+        $url_download = $url + 'tracking/' + $scheduled_id + "/download"
+        $headers = @{
+            'Authorization' = "Bearer " + $access_token
+            'content-type' = "application/json"
+        }
+        $downloaded_report = Invoke-RestMethod -Uri $url_download -Method Get -Headers $headers -ErrorAction Stop
+	    Write-Progress -Activity "Downloading report" -Status "Report downloaded" -PercentComplete 75
+        return $downloaded_report
+    } catch {
+        Write-Error "Error downloading report: $($_.Exception.Message)"
+        return $null
+    }
+}
+
+function Save-Report($out_file_name, $downloaded_report) {
+    try {
+        $downloaded_report | ConvertFrom-Csv | Export-Csv -Path $out_file_name -NoTypeInformation
+	    Write-Progress -Activity "Saving report" -Status "Report saved" -PercentComplete 100
+    } catch {
+        Write-Error "Error saving report: $($_.Exception.Message)"
+    }
+}
+
+function Main() {
+    $BaseApiUrl = "https://api." + $Region + ".data.vmwservices.com/v1/reports/"
+    $BaseAuthUrl = "https://auth." + $Region + ".data.vmwservices.com/oauth/token"
+    $access_token = Get-AccessToken $BaseAuthUrl $ClientId $ClientSecret # generate access token
+    Run-Report $ReportId $BaseApiUrl $access_token # run report
+    $report_list = Get-Report-List $ReportId $BaseApiUrl $access_token # get the list of all available reports for downloads
+    $scheduled_id = $report_list[0].id # get the latest available report
+    $downloaded_report = Download-Report $scheduled_id $BaseApiUrl $access_token # download report
+    Save-Report $OutFile $downloaded_report # save report
+}
+
+Main
+
